4e8861863bce34cabf4297c9999c7037df9676e2
Marijana Tatalovic <tatalovicmarijana@gmail.com>
2020-03-21 12:10:18 +0100

    design

---
+++ resources/views/_panel.blade.php
@@ -0,0 +1,14 @@
+<div class="border border-blue-400 rounded-lg py-6 px-8 mb-8">
+              <form action="" method="post">
+                  <textarea name="body" class="w-full" placeholder="what's up"></textarea>
+                  <hr my-4>          
+                  <footer class="flex justify-between p-4">
+                    <img src="https://i.pravatar.cc/40" 
+                    alt=""
+                    class="rounded-full mr-2"
+                    >
+                    <button type="submit" class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">Tweet!</button>
+                  </footer>
+                 
+            </form>
+          </div>
