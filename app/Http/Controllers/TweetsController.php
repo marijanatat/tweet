b887de746eef6909bdb30f484e4345669229f63e
Marijana Tatalovic <tatalovicmarijana@gmail.com>
2020-03-27 20:17:08 +0100

    render all tweets

--- app/Http/Controllers/TweetsController.php
+++ app/Http/Controllers/TweetsController.php
@@ -10,10 +10,18 @@ class TweetsController extends Controller
     public function store()
     {
         request()->validate(['body'=>'required|max:255']);
+        
         Tweet::create([
            'user_id'=>auth()->id(),
             'body'=>request('body')
         ]);
         return redirect('/home');
+    }
+    public function index()
+    {
+        $tweets=Tweet::latest()->get();
+        return view('home',[
+            'tweets'=> auth()->user()->timeline()
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-Route::post('/tweets','TweetsController@store');
+Route::middleware('auth')->group(function(){
+    Route::post('/tweets','TweetsController@store');
+Route::get('/tweets','TweetsController@index');
+});
+
